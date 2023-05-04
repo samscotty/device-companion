7e74a5167b2dc09ccd83fc099df22d6a8ac06bc6
Sam Scott <sam.scott7@me.com>
2023-05-04 19:49:48 +0100

    Implement device interface
    Device I/O and ACK listener.

---
+++ src/device_companion/watcher.py
@@ -0,0 +1,50 @@
+from collections import deque
+from dataclasses import dataclass
+from threading import Event, Thread
+
+from .domain import Command, Error, Message, Response
+
+
+@dataclass
+class WatchedCommand:
+    command: Command
+    event: Event
+
+
+class AckWatcher:
+    def __init__(self) -> None:
+        self._queue: deque[WatchedCommand] = deque()
+
+    def _watch(self, watched: WatchedCommand) -> None:
+        if not watched.event.wait(watched.command.timeout):
+            self._queue.clear()
+            self(Error(Message(f"{watched.command.message.string} not acknowledged"), TimeoutError))
+
+    def __call__(self, communication: Command | Response | Error) -> None:
+        if isinstance(communication, Error):
+            raise communication.exception(communication.message.string)
+        elif isinstance(communication, Command):
+            # Do not watch if the command has no acknowledgements
+            if not communication.acks:
+                return None
+
+            event = Event()
+            command = WatchedCommand(communication, event)
+            self._queue.append(command)
+            thread = Thread(target=self._watch, args=(command,))
+            thread.start()
+            thread.join()
+            return None
+        elif not self._queue:
+            return None
+
+        # Get the next watched command from the queue
+        watched = self._queue.popleft()
+
+        for ack in watched.command.acks:
+            if ack(communication.message.string):
+                watched.event.set()
+                break
+        else:
+            # Return to priority in the queue if no matches
+            self._queue.appendleft(watched)
