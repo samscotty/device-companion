4b6fc437388b331fce52e3c9c6637ee4c2e969f9
Sam Scott <sam.scott7@me.com>
2023-05-04 17:37:34 +0100

    Type composition of communication events
    Implements a new reusable Message type.

--- src/device_companion/domain.py
+++ src/device_companion/domain.py
@@ -1,30 +1,33 @@
 import dataclasses
-from abc import ABC
 from datetime import datetime
 from typing import Callable, Type
 
 
 @dataclasses.dataclass
-class Communication(ABC):
+class Message:
+    string: str
     timestamp: str = dataclasses.field(init=False)
 
     def __post_init__(self):
         self.timestamp = datetime.now().isoformat()
 
+    def serialise(self):
+        return dataclasses.asdict(self)
+
 
 @dataclasses.dataclass
-class Command(Communication):
-    string: str
+class Command:
+    message: Message
     timeout: float = 5.0
     acks: list[Callable[[str], bool]] = dataclasses.field(default_factory=list)
 
 
 @dataclasses.dataclass
-class Response(Communication):
-    string: str
+class Response:
+    message: Message
 
 
 @dataclasses.dataclass
-class Error(Communication):
-    string: str
+class Error:
+    message: Message
     exception: Type[Exception] = RuntimeError
