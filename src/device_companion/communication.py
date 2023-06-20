4db87399982b704d0d15f67188eeda27ed366d9e
samscotty <40459538+samscotty@users.noreply.github.com>
2023-06-20 22:53:43 +0100

    Implement a composite type for ACKs

--- src/device_companion/communication.py
+++ src/device_companion/communication.py
@@ -4,42 +4,56 @@ from datetime import datetime
 from typing import Callable, Type
 
 
-@dataclasses.dataclass()
+@dataclasses.dataclass
 class Message:
-    string: str | None
+    payload: str | None
     timestamp: str = dataclasses.field(init=False)
+
+    def serialise(self) -> str:
+        """Serialise message to a JSON formatted `str`."""
+        obj = dataclasses.asdict(self)
+        return json.dumps(obj)
+
+    @classmethod
+    def deserialise(cls, obj: str | bytes | bytearray) -> "Message":
+        """Deserialise JSON object to a `Message`."""
+        try:
+            message = json.loads(obj)
+            payload = str(message["payload"])
+        except (KeyError, TypeError, ValueError):
+            return cls(None)
+        else:
+            return cls(payload)
 
     def __post_init__(self):
         self.timestamp = datetime.now().isoformat()
 
-    def serialise(self) -> str:
-        payload = dataclasses.asdict(self)
-        return json.dumps(payload)
-
-    @classmethod
-    def deserialise(cls, obj: str | bytes | bytearray) -> "Message":
-        try:
-            payload = json.loads(obj)
-            message = str(payload["string"])
-        except (KeyError, TypeError, ValueError):
-            return cls(None)
-        else:
-            return cls(message)
-
     def __bool__(self) -> bool:
-        return bool(self.string)
-
-
-@dataclasses.dataclass
-class Command:
-    message: Message
-    timeout: float = 5.0
-    acks: list[Callable[[Message], bool]] = dataclasses.field(default_factory=list)
+        return bool(self.payload)
 
 
 @dataclasses.dataclass
 class Response:
     message: Message
+
+
+@dataclasses.dataclass
+class Acknowledgement:
+    matchers: list[Callable[[Response], bool]] = dataclasses.field(default_factory=list)
+    timeout: float = 5.0
+
+    def __post_init__(self):
+        if not self.matchers or self.timeout < 0:
+            self.timeout = 0.0
+
+    def __bool__(self) -> bool:
+        return bool(self.matchers)
+
+
+@dataclasses.dataclass
+class Command:
+    message: Message
+    ack: Acknowledgement = Acknowledgement()
 
 
 @dataclasses.dataclass
